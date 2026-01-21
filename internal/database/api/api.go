5f20706ada2987a9ff74f5a5451cc997635e85a1
Lior Aronovich <lioraronpr@gmail.com>
2026-01-21 14:27:24 -0500

    dev

--- internal/database/api/api.go
+++ internal/database/api/api.go
@@ -8,57 +8,12 @@ import (
 	"github.com/llm-d-incubation/batch-gateway/internal/util/logging"
 )
 
-type BatchDBClient interface {
+// type BatchDBClient interface {
 
-	// Store stores a batch job, and adds the job to the processing queue.
-	// Returns the ID of the job in the database.
-	Store(ctx context.Context, logger logging.Logger, job *BatchJob) (ID string, err error)
-
-	// GetForProcessing retrieves batch jobs for processing.
-	// This function blocks for the duration specified by timeout. If timeout is zero, the function does not block.
-	// This function will attempt to pull the maximum number of jobs specified in maxJobs.
-	// Returns a list of jobs and a list of event channels, corresponding to the jobs.
-	// For each returned job whose processing is finished by the caller - the caller MUST call the function
-	// CloseFn specified in BatchEventsChan, to close the associated resources.
-	GetForProcessing(ctx context.Context, logger logging.Logger, timeout time.Duration, maxJobs int) (
-		jobs []*BatchJob, events []*BatchEventsChan, err error)
-
-	// Update updates the dynamic parts of a batch job, and optionally adds the job ID back to the processing queue.
-	// The function will update in the job's record in the database - all the dynamic fields of the job which are not empty in the given job object.
-	// Any dynamic field that is empty in the given job object - will not be updated in the job's record in the database.
-	// If 'addJobToQueue' is set to true then the job's object SLO field must be non empty, and the function will
-	// add the job's ID to the processing queue.
-	Update(ctx context.Context, logger logging.Logger, job *BatchJob, addJobToQueue bool) (err error)
-
-	// Get gets the information (static and dynamic) of batch jobs.
-	// If IDs are specified, this function will get jobs by the specified IDs.
-	// If tags are specified, this function will get jobs by the specified tags.
-	// If no IDs nor tags are specified, the function will return an empty list of jobs.
-	// tagsLogicalCond specifies the logical condition to use for when searching for the tags per job.
-	// includeStatic specifies if to include the static part of a job in the returned output.
-	// start and limit specify the pagination details. This is relevant only for search by tags.
-	// In the first iteration with pagination specify 0 for 'start', and in any subsequent iteration specify in 'start'
-	// the value that was returned by 'cursor' in the previous iteration. The value returned by 'cursor' is an opaque integer.
-	// The value specified in 'limit' can be different between iterations, and is a recommendation only.
-	// jobs is a slice of returned jobs.
-	// cursor is an opaque integer that should be given in the next paginated call via the 'start' parameter.
-	Get(ctx context.Context, logger logging.Logger, IDs []string, tags []string, tagsLogicalCond TagsLogicalCond,
-		includeStatic bool, start, limit int) (
-		jobs []*BatchJob, cursor int, err error)
-
-	// SendEvent sends events for the specified jobs.
-	// The events are sent and consumed in FIFO order.
-	SendEvent(ctx context.Context, logger logging.Logger, events []BatchEvent) (sentIDs []string, err error)
-
-	// Delete deletes batch jobs.
-	Delete(ctx context.Context, logger logging.Logger, IDs []string) (deletedIDs []string, err error)
-
-	// Get default context for the DB request.
-	GetContext(parentCtx context.Context, timeLimit time.Duration) (context.Context, context.CancelFunc)
-
-	// Close closes the client.
-	Close() error
-}
+// 	// SendEvent sends events for the specified jobs.
+// 	// The events are sent and consumed in FIFO order.
+// 	SendEvent(ctx context.Context, logger logging.Logger, events []BatchEvent) (sentIDs []string, err error)
+// }
 
 type BatchJob struct {
 	ID     string    // [mandatory, immutable, returned by get, parsed by DB, must be unique] User provided unique ID of the job. This ID must be unique.
@@ -80,6 +35,68 @@ func (bj *BatchJob) IsValid() error {
 		return fmt.Errorf("TTL is invalid for ID %s", bj.ID)
 	}
 	return nil
+}
+
+// BatchDBClient enables to manage batch job metadata objects in persistent storage.
+type BatchDBClient interface {
+
+	// Store stores a batch job.
+	// Returns the ID of the job in the database.
+	Store(ctx context.Context, logger logging.Logger, job *BatchJob) (jobID string, err error)
+
+	// Get gets the information (static and dynamic) of batch jobs.
+	// If IDs are specified, this function will get jobs by the specified IDs.
+	// If tags are specified, this function will get jobs by the specified tags.
+	// If no IDs nor tags are specified, the function will return an empty list of jobs.
+	// tagsLogicalCond specifies the logical condition to use for when searching for the tags per job.
+	// includeStatic specifies if to include the static part of a job in the returned output.
+	// start and limit specify the pagination details. This is relevant only for search by tags.
+	// In the first iteration with pagination specify 0 for 'start', and in any subsequent iteration specify in 'start'
+	// the value that was returned by 'cursor' in the previous iteration. The value returned by 'cursor' is an opaque integer.
+	// The value specified in 'limit' can be different between iterations, and is a recommendation only.
+	// jobs is a slice of returned jobs.
+	// cursor is an opaque integer that should be given in the next paginated call via the 'start' parameter.
+	Get(ctx context.Context, logger logging.Logger, jobIDs []string, tags []string, tagsLogicalCond TagsLogicalCond,
+		includeStatic bool, start, limit int) (
+		jobs []*BatchJob, cursor int, err error)
+
+	// Update updates the dynamic parts of a batch job.
+	// The function will update in the job's record in the database - all the dynamic fields of the job which are not empty
+	// in the given job object.
+	// Any dynamic field that is empty in the given job object - will not be updated in the job's record in the database.
+	Update(ctx context.Context, logger logging.Logger, job *BatchJob) (err error)
+
+	// Delete deletes batch jobs.
+	Delete(ctx context.Context, logger logging.Logger, jobIDs []string) (deletedJobIDs []string, err error)
+
+	// Get a default derived context for a DB request.
+	GetContext(parentCtx context.Context, timeLimit time.Duration) (context.Context, context.CancelFunc)
+
+	// Close closes the client.
+	Close() error
+}
+
+type BatchJobPriority struct {
+	ID  string
+	SLO time.Time // The SLO value determines the priority of the job id.
+}
+
+// BatchPriorityQueueClient enables to perform operation on a priority queue.
+type BatchPriorityQueueClient interface {
+
+	// Enqueue adds a job priority object to the queue.
+	Enqueue(ctx context.Context, logger logging.Logger, jobPriority *BatchJobPriority) error
+
+	// Dequeue returns the job priority object at the head of the queue.
+	// The function blocks up to the timeout value for a job priority object to be available.
+	// If the timeout value is zero, the function returns immediately.
+	Dequeue(ctx context.Context, logger logging.Logger, timeout time.Duration) (jobPriority *BatchJobPriority, err error)
+
+	// Remove deletes a job priority object from the queue.
+	Remove(ctx context.Context, logger logging.Logger, jobPriority *BatchJobPriority) error
+
+	// Close closes the client.
+	Close() error
 }
 
 type BatchEventType int
@@ -116,6 +133,27 @@ type BatchEventsChan struct {
 	CloseFn func()          // Function for closing the channel and the associated resources. Must be called by the listener when the job's processing is finished.
 }
 
+// map to whatever the implementation is
+type BatchEventChannelClient interface {
+
+	// 	// GetForProcessing retrieves batch jobs for processing.
+	// 	// This function blocks for the duration specified by timeout. If timeout is zero, the function does not block.
+	// 	// This function will attempt to pull the maximum number of jobs specified in maxJobs.
+	// 	// Returns a list of jobs and a list of event channels, corresponding to the jobs.
+	// 	// For each returned job whose processing is finished by the caller - the caller MUST call the function
+	// 	// CloseFn specified in BatchEventsChan, to close the associated resources.
+	// 	GetForProcessing(ctx context.Context, logger logging.Logger, timeout time.Duration, maxJobs int) (
+	// 		jobs []*BatchJob, events []*BatchEventsChan, err error)
+
+	// Get gets an event channel based on the jobId and runId
+	// CloseFn specified in BatchEventsChan, to close the associated resources.
+	Get(ctx context.Context, logger logging.Logger, jobID string) (event BatchEventsChan, err error)
+
+	// Send sends events for the specified jobs.
+	// The events are sent and consumed in FIFO order.
+	Send(ctx context.Context, logger logging.Logger, events BatchEvent) (sentID string, err error)
+}
+
 type TagsLogicalCond int
 
 const (
@@ -131,74 +169,6 @@ var TagsLogicalCondNames = map[TagsLogicalCond]string{
 }
 
 //----
-
-// BatchDB is responsible for persisting batch metadata.
-type BatchDB interface {
-	// Store saves a new batch run metadata.
-	Store(ctx context.Context, logger logging.Logger, runMeta *RunMetadata, DBtype string, DBOptions any) (runId string, err error)
-
-	// List batch runs by conditions
-	List(ctx context.Context, logger logging.Logger, DBtype string, DBOptions any) (runs []*RunMetadata, next int, err error)
-
-	// Get run by id
-	Get(ctx context.Context, logger logging.Logger, DBtype string, DBOptions any) (run *RunMetadata, err error)
-
-	// Update modifies mutable fields of a batch run.
-	Update(ctx context.Context, logger logging.Logger, run *RunMetadata, DBtype string, DBOptions any) (err error)
-
-	// Delete removes batch run metadata.
-	Delete(ctx context.Context, logger logging.Logger, DBtype string, DBOptions any) (deletedId string, err error)
-
-	// Cancel marks batch run as canceled.
-	Cancel(ctx context.Context, logger logging.Logger, DBtype string, DBOptions any) (canceledId string, err error)
-
-	// GetContext returns a derived context with a timeout for DB operations.
-	GetContext(parentCtx context.Context, timeLimit time.Duration) (context.Context, context.CancelFunc)
-
-	// Close releases any resources held by the implementation.
-	Close() error
-}
-
-type RunMetadata struct {
-	Id     string // [mandatory, immutable, returned by get, parsed by DB, must be unique] User provided unique ID of the run. This ID must be unique.
-	Spec   []byte // [optional, immutable, returned optionally by get, opaque to DB] The static part of the batch run (serialized), including the run's specification.
-	Status []byte // [optional, updatable, returned by get, opaque to DB] The dynamic part of the batch run (serialized), including its status.
-}
-
-type JobsOptions struct {
-	UserFs bool
-	JobId  string
-	runId  string
-	Start  string
-	Limit  int
-	States []string
-}
-
-// RunQueue handles enqueuing, dequeuing and removal of runs from the processing queue.
-type RunQueue interface {
-	// Enqueue adds a run id to the processing queue.
-	Enqueue(ctx context.Context, logger logging.Logger, jobId, runId string, SLO time.Time) error
-
-	// Dequeue blocks up to timeout for a run id to become available.
-	// If timeout is zero, it returns immediately with no runs.
-	Dequeue(ctx context.Context, logger logging.Logger, timeout time.Duration) (jobId, runId string, err error)
-
-	// Remove deletes a run id from the queue (e.g., when a run is cancelled).
-	Remove(ctx context.Context, logger logging.Logger, runId string) error
-
-	// Close releases any resources held by the implementation.
-	Close() error
-}
-
-// map to whatever the implementation is
-type EventChannel interface {
-	// Get gets an event channel based on the jobId and runId
-	// CloseFn specified in BatchEventsChan, to close the associated resources.
-	Get(ctx context.Context, logger logging.Logger, jobId, runId string) (event BatchEventsChan, err error)
-	// Send sends events for the specified jobs.
-	// The events are sent and consumed in FIFO order.
-	Send(ctx context.Context, logger logging.Logger, events BatchEvent) (sentID string, err error)
-}
 
 // RunProgressDB stores temporary per‑job progress information.
 // Entries are expected to be removed once the job finishes.
