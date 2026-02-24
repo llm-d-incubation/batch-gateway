ff15dc9e0480bb4c28a05a9d2178f61bf82bb130
Lior Aronovich <lioraronpr@gmail.com>
2026-02-24 18:55:26 -0500

    dev

--- internal/database/redis/redis_get_by_ids.lua
+++ internal/database/redis/redis_get_by_ids.lua
@@ -12,7 +12,7 @@
 -- See the License for the specific language governing permissions and
 -- limitations under the License.
 
--- Get by expiry lua script.
+-- Get by IDs lua script.
 
 -- Parse inputs.
 local IDs = KEYS
--- internal/database/redis/redis_get_by_tenant.lua
+++ internal/database/redis/redis_get_by_tenant.lua
@@ -12,7 +12,7 @@
 -- See the License for the specific language governing permissions and
 -- limitations under the License.
 
--- Get by expiry lua script.
+-- Get by tenant lua script.
 
 -- Parse inputs.
 local tenantId = ARGV[1]
