139921d8408717e976df6c9108fa6a174a61129c
Lior Aronovich <lioraronpr@gmail.com>
2026-03-20 18:45:24 -0400

    update dev-clean.sh

--- scripts/dev-clean.sh
+++ scripts/dev-clean.sh
@@ -16,6 +16,12 @@ cleanup_kubernetes_resources() {
     helm uninstall "${REDIS_RELEASE}" -n "${NAMESPACE}" 2>/dev/null || true
     helm uninstall "${POSTGRESQL_RELEASE}" -n "${NAMESPACE}" 2>/dev/null || true
 
+    # Delete NodePort services (created outside of Helm)
+    log "Deleting NodePort services..."
+    kubectl delete svc "${HELM_RELEASE}-apiserver-nodeport" -n "${NAMESPACE}" --ignore-not-found=true
+    kubectl delete svc "${HELM_RELEASE}-processor-nodeport" -n "${NAMESPACE}" --ignore-not-found=true
+    kubectl delete svc "${PROMETHEUS_NAME}-nodeport" -n "${NAMESPACE}" --ignore-not-found=true
+
     # Delete deployments and services
     log "Deleting deployments and services..."
     kubectl delete deployment,svc "${JAEGER_NAME}" -n "${NAMESPACE}" --ignore-not-found=true
